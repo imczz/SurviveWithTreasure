06d404da8016c9de2c8f7afa66b47abe4d6926ef
imczz <790608522@qq.com>
2018-12-07 10:47:20 +0800

    :bug:更正HashMap比较大小的函数，增加单元测试

--- src/czz/testSWT/OfferAndDemandTest.java
+++ src/czz/testSWT/OfferAndDemandTest.java
@@ -40,4 +40,77 @@ class OfferAndDemandTest {
 		assertArrayEquals(expectedArr, actualArr);
 	}
 	
+	@Test
+	void testBatch() {
+		HashMap<Integer, Integer> values = new HashMap<Integer, Integer>();
+		values.put(1, 2);
+		values.put(2, 3);
+		values.put(3, 4);
+		Demand demand = new Demand(0, values);
+		values.put(1, 0);
+		values.put(2, 1);
+		values.put(3, -1);
+		assertTrue(demand.batch(values));
+		assertEquals(2, demand.getValueById(1));
+		assertEquals(4, demand.getValueById(2));
+		assertEquals(3, demand.getValueById(3));
+		values.put(1, 0);
+		values.put(2, -4);
+		values.put(3, 2);
+		assertTrue(demand.batch(values));
+		assertEquals(2, demand.getValueById(1));
+		assertEquals(0, demand.getValueById(2));
+		assertEquals(5, demand.getValueById(3));
+		values.put(1, -4);
+		values.put(2, 0);
+		values.put(3, 0);
+		assertFalse(demand.batch(values));
+		assertEquals(2, demand.getValueById(1));
+		assertEquals(0, demand.getValueById(2));
+		assertEquals(5, demand.getValueById(3));
+	}
+	
+	@Test
+	void testContains() {
+		HashMap<Integer, Integer> values1 = new HashMap<Integer, Integer>();
+		values1.put(1, 2);
+		values1.put(2, 3);
+		values1.put(3, 4);
+		Demand demand1 = new Demand(0, values1);
+		HashMap<Integer, Integer> values2 = new HashMap<Integer, Integer>();
+		values2.put(1, 2);
+		values2.put(2, 3);
+		values2.put(3, 4);
+		Demand demand2 = new Demand(1, values2);
+		HashMap<Integer, Integer> values3 = new HashMap<Integer, Integer>();
+		values3.put(1, 2);
+		values3.put(2, 3);
+		Demand demand3 = new Demand(0, values3);
+		HashMap<Integer, Integer> values4 = new HashMap<Integer, Integer>();
+		values4.put(1, 2);
+		values4.put(2, 3);
+		values4.put(3, 3);
+		Demand demand4 = new Demand(0, values4);
+		HashMap<Integer, Integer> values5 = new HashMap<Integer, Integer>();
+		values5.put(1, 2);
+		values5.put(2, 3);
+		values5.put(4, 3);
+		Demand demand5 = new Demand(0, values5);
+		HashMap<Integer, Integer> values6 = new HashMap<Integer, Integer>();
+		values6.put(1, 2);
+		values6.put(5, 3);
+		Demand demand6 = new Demand(0, values6);
+		HashMap<Integer, Integer> values7 = new HashMap<Integer, Integer>();
+		values7.put(1, 2);
+		values7.put(2, 4);
+		Demand demand7 = new Demand(0, values7);
+		assertTrue(demand1.contains(demand2));
+		assertTrue(demand2.contains(demand1));
+		assertTrue(demand1.contains(demand3));
+		assertTrue(demand1.contains(demand4));
+		assertFalse(demand1.contains(demand5));
+		assertFalse(demand1.contains(demand6));
+		assertFalse(demand1.contains(demand7));
+	}
+	
 }
--- src/czz/util/MyCompare.java
+++ src/czz/util/MyCompare.java
@@ -41,28 +41,54 @@ public class MyCompare {
 	 * @return 0两个HashMap完全相同;1amap包含bmap（b是a的子集）;-1amap包含于bmap（a是b的子集）;-2无法比较
 	 * */
 	public static <K, V extends Comparable<V>> int compairHashMap(AbstractMap<K, V> mapa, AbstractMap<K, V> mapb) {
-		int ret = -1;
+		int ret = -2;
+		boolean aSize = true;		//a比b大
+		boolean sizeEqual = false;
 		if (mapa == null && mapb == null) ret = 0;				//均为空集
-		else if (mapa != null && mapb != null && mapa.size() == mapb.size()){
+		else if (mapa != null && mapb != null) {
+			AbstractMap<K, V> a, b = null;
+			if (mapa.size() >= mapb.size()){						//a的容量比b大
+				if (mapa.size() == mapb.size()) sizeEqual = true;		//ab长度相同
+				a = mapa;					//大的一个
+				b = mapb;					//小的一个
+				aSize = true;
+			} else {
+				a = mapb;
+				b = mapa;
+				aSize = false;
+			}
 			ret = 0;
-			Iterator<Entry<K, V>> iter = mapa.entrySet().iterator();
+			Iterator<Entry<K, V>> iter = b.entrySet().iterator();			//遍历小的集合
 			Entry<K, V> entry = null;
+			V aValue = null;
 			while (iter.hasNext()) {
 				entry = iter.next();
-				if (entry.getValue().compareTo(mapb.get(entry.getKey())) > 0) {
-					if (ret == 0) {
-						ret = 1;
-					} else {
-						ret = -2;
-						break;
+				aValue = a.get(entry.getKey());
+				if (aValue == null) {				//小集合中含有大集合中没有的，一定没有包含或者被包含关系
+					ret = -2;
+					break;
+				} else {				//小集合中的每一个值大集合中都有
+					if (entry.getValue().compareTo(aValue) > 0) {
+						if (sizeEqual && ret == 0) {
+							ret = -1;
+						} else {
+							ret = -2;
+							break;
+						}
+					} else if (entry.getValue().compareTo(aValue) < 0) {
+						if (ret == 0) {
+							ret = 1;
+						} else if (ret == 1){
+							ret = -2;
+							break;
+						}
 					}
-				} else if (entry.getValue().compareTo(mapb.get(entry.getKey())) < 0) {
-					if (ret == 0) {
-						ret = -1;
-					} else {
-						ret = -2;
-						break;
-					}
+				}
+			}
+			if (ret != -2) {
+				if (!sizeEqual && ret == 0) ret = 1;			//大小不相等，仍然为子集
+				if (!aSize) {
+					ret = -ret;
 				}
 			}
 		}
